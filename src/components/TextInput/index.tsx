0755915e71aa13860e66fce736744d4d131be95e
Daniel Romero <tutorialesdanielromero@gmail.com>
2025-04-24 15:07:36 -0500

    :construction_worker: add input for login

---
+++ src/components/TextInput/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {Text, View, TextInput, KeyboardTypeOptions} from 'react-native';
+import {styles} from './styles';
+
+interface Props {
+  value: string;
+  onChangeText: (text: string) => void;
+  keyboardType?: KeyboardTypeOptions | undefined;
+  isPassword?: boolean;
+  placeHolder?: string;
+}
+
+export const TextInputCustom: React.FC<Props> = ({
+  value,
+  onChangeText,
+  keyboardType = 'default',
+  isPassword = false,
+  placeHolder
+}) => {
+  return (
+    <TextInput
+      keyboardType={keyboardType}
+      style={styles.container}
+      value={value}
+      onChangeText={onChangeText}
+      secureTextEntry={isPassword}
+      placeholder={placeHolder}
+    />
+  );
+};
---
+++ src/components/TextInput/styles.tsx
@@ -0,0 +1,16 @@
+
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#f6f6f6',
+    width: '80%',
+    borderRadius: 10,
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+    borderWidth: 1,
+    borderColor: 'gray',
+    // top: 10,
+    marginTop: 10,
+  },
+});
